cbbe010c946ab632b0ea305aabaee3c9c84579ee
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-05 01:30:55 -0500

    FEAT: Added the ability to add reviews for a teacher

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :teachers, only: [:index, :show]
+  resources :teachers, only: [:index, :show] do
+    resources :reviews, only: :create
+  end
 end
